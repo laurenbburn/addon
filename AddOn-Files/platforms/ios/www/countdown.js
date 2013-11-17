ca04b35b8b90fd7a9a9a025ac7d8529d894a9c03
evanjarzynski <evanjarzynski@gmail.com>
2013-11-17 17:29:42 -0600

    Add code to have timer change color

--- AddOn-Files/platforms/ios/www/countdown.js
+++ AddOn-Files/platforms/ios/www/countdown.js
@@ -5,10 +5,21 @@ function countdown_timer(){
   // decrease timer
   max_time--;
   document.getElementById('time').innerHTML = max_time;
-  if(max_time == 0){
+  document.getElementById('countdown').style.backgroundColor = "#000000"
+  if(max_time == 3)
+  document.getElementById('countdown').style.backgroundColor = "#400000"
+  else{if(max_time == 2){
+  	document.getElementById('countdown').style.backgroundColor = "#800000"
+  	};
+  	if(max_time == 1){
+  	document.getElementById('countdown').style.backgroundColor = "#C00000"
+  	};
+  	if(max_time == 0){
     clearInterval(cinterval);
     document.getElementById('time').innerHTML = "0";
-  }
+    document.getElementById('countdown').style.backgroundColor = "#FF0000"
+  	};
+  };
 }
 
 // 1,000 means 1 second.
