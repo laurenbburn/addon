95735d8731d7ff026ae6a920767224cabd053657
evanjarzynski <evanjarzynski@gmail.com>
2014-01-06 01:03:08 -0600

    Save inputs to database and refill forms on load

--- AddOn-Files/platforms/ios/www/js/database.js
+++ AddOn-Files/platforms/ios/www/js/database.js
@@ -1,4 +1,4 @@
-var addOn_dB, players;
+var addOn_dB, players, ID, name1, name2, name3, name4;
 
 addOn_db = loadDB("addOnDB")
 
@@ -27,9 +27,26 @@ function saveDB(name, db){
 
 //Creates a new player when the "+" button is pushed
 function create_Player(){
+	
+	addOn_db = loadDB("addOnDB")
+	var json = JSON.parse(localStorage["addOnDB"]);
+	
+	if(json.length == 2){
+		var num = 3;
+	}
+	else {
+		for(i=0;i<json.length;i++){
+			if (json[i].id == 3) {
+			var num = 4;
+			} else {
+			var num = 3;
+			}
+		}
+	}
+	
      	//Create player array with elements
 		var newPlayer = {
-			id: i+1,
+			id: num,
 			number:"−",
 			name:"",
 			color:""
@@ -41,7 +58,8 @@ function create_Player(){
 		//save database
 		saveDB("addOnDB", addOn_db)
 		//update the players
-		updatePlayers()
+		//updatePlayers()
+		check_Add()
 	}		
 
 //Deletes all players from the database when the back button is pressed.
@@ -64,6 +82,7 @@ $(function(){
 
 	// Populate the players 
 	updatePlayers();
+	check_Add();
 
 });
 
@@ -76,16 +95,6 @@ function updatePlayers(){
 		var player = addOn_db[j];
 		var row = create_Row(player);
 		players.append(row);
-	}
-	
-	//gets rid of plus button if 4 players exist
-	i = addOn_db.length;
-	var addButton = $("#add_player")
-	
-	if (i==4){
-		addButton.css('display', 'none')
-	}else{
-		addButton.css('display', 'block')
 	}
 }
 
@@ -111,7 +120,7 @@ function initial(){
 	players.append(create_Row(newPlayer1));
 	players.append(create_Row(newPlayer2));
 	saveDB("addOnDB", addOn_db)
-	updatePlayers()
+	//updatePlayers()
 
 	
 }
@@ -119,12 +128,13 @@ function initial(){
 //Creates the html li for a player
 function create_Row(player){
 	var please = player.id;
+	
 	var playerList = $("#thePlayers");
 	var row = $("<li class='with_border'>");
 	var numberButton = $("<button class='numbers' type='button'>");
-	var nameInput = $("<input type='text'>");
+	var nameInput = $("<input name = '"+please +"' type='text'>");
 	var colorSpan = $("<span class='date'>");
-	var colorLink = $("<a class= 'swatch' href= 'colors.html'>");
+	var colorLink = $("<a id = '"+please +"' class= 'swatch' href= 'colors.html'>");
 	numberButton.text(player.number);
 	nameInput.text (player.name);
 	colorLink.append(colorSpan);
@@ -140,12 +150,86 @@ function create_Row(player){
 		for (i=0;i<json.length;i++){
             if (json[i].id == please) json.splice(i,1);
 			localStorage["addOnDB"] = JSON.stringify(json);}
+			//Get rid of player from db
 			addOn_db.pop(player);
-
-			updatePlayers()
+			//remove the row
+			row.remove();
+			
+			check_Add();
+			
 	});
 
 	return row;
 }
 
+//when swatch is clicked grabs the id and runs save_Names()
+$(document).on("click", ".swatch", function(e) {
+    alert(this.id);
+    save_Names();
+    });
+    
+//Saves input values as names in the database
+function save_Names(){
+    name1 = $("[name='1']").val();
+	name2 = $("[name='2']").val();
+	name3 = $("[name='3']").val();
+	name4 = $("[name='4']").val();
+	
+	function setItemName(json){
+		for (var i in json){
+			if(json[i].id == 1){
+				json[i].name = name1;
+			}else if(json[i].id == 2){
+				json[i].name = name2;
+			}else if(json[i].id == 3){
+				json[i].name = name3;
+			}else{
+				json[i].name = name4;
+			}
+		}
+		return json;
+	};
+	
+	addOn_db = loadDB("addOnDB")
+	var json = JSON.parse(localStorage["addOnDB"]);
+	for(i=0;i<json.length;i++){
+	json = setItemName(json);
+	console.log(json);
+	localStorage["addOnDB"] = JSON.stringify(json);}
+    }
+
+
+
+//gets rid of plus button if 4 players exist	
+function check_Add(){
+	i = addOn_db.length;
+	var addButton = $("#add_player")
+	
+	if (i==4){
+		addButton.css('display', 'none')
+	}else{
+		addButton.css('display', 'block')
+	}
+}
+
+//fills input forms with values from the database onload
+$(document).ready(function(){
+	addOn_db = loadDB("addOnDB")
+	var json = JSON.parse(localStorage["addOnDB"]);
+	for (var i in json){
+			if(json[i].id == 1){
+				name1 = json[i].name
+			}else if(json[i].id == 2){
+				name2 = json[i].name
+			}else if(json[i].id == 3){
+				name3 = json[i].name
+			}else{
+				name4 = json[i].name
+			}
+		}
+	$("[name='1']").val(name1);
+	$("[name='2']").val(name2);
+	$("[name='3']").val(name3);
+	$("[name='4']").val(name4);
+});
 
